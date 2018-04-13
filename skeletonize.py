6dbc044c67724a8a3ac2deb3ad01a981a72e2e0c
nulman <nulman.alex@gmail.com>
2018-04-13 22:52:05 +0300

    more skeletonize things
    tried de-normalizing before triangulating
    denormilizing triangulated points
    added todo

--- skeletonize.py
+++ skeletonize.py
@@ -2,6 +2,7 @@ import camera_calibrate
 import numpy as np
 import cv2
 import sys
+#adjust this to reflect where you have tf-openpose
 sys.path.append('E:\\cygwin64\\home\\Alex\\git\\tf-openpose')
 sys.path.append('E:\\cygwin64\\home\\Alex\\git\\tf-openpose\\src')
 import common
@@ -19,13 +20,17 @@ def prep_model():
     e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
     return {'e': e, 'scales': scales}
 
-def get_skeleton(im, e, scales):
+def get_skeleton(im, e, scales, shape=None):
     # estimate human poses from a single image !
     image = common.read_imgfile(im, None, None)
+    if shape is not None:
+        shape.clear()
+        shape.extend(image.shape[:-1])
     humans = e.inference(image, scales=scales) #list of skeletons
     # image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False) #"draws" the skeletons on the image
     return humans
 
+#the path here should point to the directory with the calibration pics
 calibration = camera_calibrate.StereoCalibration(r'Z:\intermitent data\frames\\', False)
 
 A_RT_left = calibration.camera_model['M1'].dot(
@@ -35,14 +40,24 @@ A_RT_right = calibration.camera_model['M2'].dot(
 
 
 model = prep_model()
-lhumans = get_skeleton(r'Z:\intermitent data\poses\L_2.png', **model)
-rhumans = get_skeleton(r'Z:\intermitent data\poses\R_2.png', **model)
+im_shape = []
+lhumans = get_skeleton(r'Z:\intermitent data\poses\L_3.png', shape=im_shape, **model)
+rhumans = get_skeleton(r'Z:\intermitent data\poses\R_3.png', **model)
 lparts = []
 rparts = []
 for matching_part in list(set(rhumans[0].body_parts.keys()).intersection(lhumans[0].body_parts.keys())):
-    rparts.append([rhumans[0].body_parts[matching_part].x, rhumans[0].body_parts[matching_part].y])
-    lparts.append([lhumans[0].body_parts[matching_part].x, lhumans[0].body_parts[matching_part].y])
+    #maybe undistort before appending?
+    rparts.append([rhumans[0].body_parts[matching_part].x*im_shape[0],
+                   rhumans[0].body_parts[matching_part].y*im_shape[1]])
+    lparts.append([lhumans[0].body_parts[matching_part].x*im_shape[0],
+                   lhumans[0].body_parts[matching_part].y*im_shape[1]])
+
 
 lparts = np.array(lparts).reshape([2, -1])
 rparts = np.array(rparts).reshape([2, -1])
-cv2.triangulatePoints(A_RT_left, A_RT_right, lparts, rparts)
+triangulated_4d = cv2.triangulatePoints(A_RT_left, A_RT_right, lparts, rparts).reshape([-1, 4])
+triangulated_3d = triangulated_4d[:, :3] / triangulated_4d[:, -1, np.newaxis]
+print(triangulated_3d)
+
+#todo investigate https://github.com/markjay4k/3D-Pose-Estimation/blob/master/pt4-webcam3D.py and his use of Prob3dPose
+#todo which i think comes from https://github.com/DenisTome/Lifting-from-the-Deep-release
