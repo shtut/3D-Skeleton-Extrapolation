e41a70d5eb27bd1ba9511e4664c32e0a880cf8f6
Hadas <shtut@users.noreply.github.com>
2018-09-05 02:08:00 +0300

    .

--- Code/compare.m
+++ Code/compare.m
@@ -10,13 +10,12 @@ function  [] = compare(calibration, fig1, fig2)
 kinect = load(fig2);
 %calculate openpose 3D data
 [OpenPoseSkeleton3D, orderOP] = triangulateOpenpose(calibration, fig1);
-% remove center of mass from all the points (move to 0,0)
 center = mean(OpenPoseSkeleton3D);
 OpenPoseSkeleton3D = OpenPoseSkeleton3D - center;
 
 % Split points to start and end points
-OP_start2 = OpenPoseSkeleton3D(1:2:end,:);
-OP_end2 = OpenPoseSkeleton3D(2:2:end,:);
+OP_start = OpenPoseSkeleton3D(1:2:end,:);
+OP_end = OpenPoseSkeleton3D(2:2:end,:);
 
 kinect_start = kinect.lines(1:2:end,:);
 kinect_end = kinect.lines(2:2:end,:);
@@ -24,27 +23,65 @@ kinect_end = kinect.lines(2:2:end,:);
 orderK = cellstr(kinect.order);
 orderOP = cellstr(orderOP);
 % % % get kinect and openpose left foot point
+OPhead = OpenPoseSkeleton3D(find(contains(orderOP,"Nose"),1,'first'),:);
+OPneck = OpenPoseSkeleton3D(find(contains(orderOP,"Neck"),1,'first'),:);
+OPshoulder1 = OpenPoseSkeleton3D(find(contains(orderOP,"LShoulder"),1,'first'),:);
+OPshoulder2 = OpenPoseSkeleton3D(find(contains(orderOP,"RShoulder"),1,'first'),:);
+OPelbow1 = OpenPoseSkeleton3D(find(contains(orderOP,"LElbow"),1,'first'),:);
+OPelbow2 = OpenPoseSkeleton3D(find(contains(orderOP,"RElbow"),1,'first'),:);
+OPhand1 = OpenPoseSkeleton3D(find(contains(orderOP,"LWrist"),1,'first'),:);
+OPhand2 = OpenPoseSkeleton3D(find(contains(orderOP,"RWrist"),1,'first'),:);
+OPhip1 = OpenPoseSkeleton3D(find(contains(orderOP,"LHip"),1,'first'),:);
+OPhip2 = OpenPoseSkeleton3D(find(contains(orderOP,"RHip"),1,'first'),:);
+OPknee1 = OpenPoseSkeleton3D(find(contains(orderOP,"LKnee"),1,'first'),:);
+OPknee2 = OpenPoseSkeleton3D(find(contains(orderOP,"RKnee"),1,'first'),:);
 OPfoot1 = OpenPoseSkeleton3D(find(contains(orderOP,"LAnkle"),1,'first'),:);
 OPfoot2 = OpenPoseSkeleton3D(find(contains(orderOP,"RAnkle"),1,'first'),:);
-OPneck = OpenPoseSkeleton3D(find(contains(orderOP,"Nose"),1,'first'),:);
-% OPelbow = OpenPoseSkeleton3D(find(contains(orderOP,"RElbow"),1,'first'),:);
 
-Kfoot1 = kinect.lines(find(contains(orderK,"FootLeft"),1,'first'),:);
-Kfoot2 = kinect.lines(find(contains(orderK,"FootRight"),1,'first'),:);
-Kneck = kinect.lines(find(contains(orderK,"Neck"),1,'first'),:);
-% Kelbow = kinect.lines(find(contains(orderK,"ElbowLeft"),1,'first'),:);
+Khead = kinect.lines(find(contains(orderK,"Neck"),1,'first'),:);
+Kneck = kinect.lines(find(contains(orderK,"SpineShoulder"),1,'first'),:);
+Kshoulder1 = kinect.lines(find(contains(orderK,"ShoulderLeft"),1,'first'),:);
+Kshoulder2 = kinect.lines(find(contains(orderK,"ShoulderRight"),1,'first'),:);
+Kelbow1 = kinect.lines(find(contains(orderK,"ElbowLeft"),1,'first'),:);
+Kelbow2 =kinect.lines(find(contains(orderK,"ElbowRight"),1,'first'),:);
+Khand1 = kinect.lines(find(contains(orderK,"WristLeft"),1,'first'),:);
+Khand2 = kinect.lines(find(contains(orderK,"WristRight"),1,'first'),:);
+Khip1 = kinect.lines(find(contains(orderK,"HipLeft"),1,'first'),:);
+Khip2 = kinect.lines(find(contains(orderK,"HipRight"),1,'first'),:);
+Kknee1 = kinect.lines(find(contains(orderK,"KneeLeft"),1,'first'),:);
+Kknee2 = kinect.lines(find(contains(orderK,"KneeRight"),1,'first'),:);
+Kfoot1 = kinect.lines(find(contains(orderK,"AnkleLeft"),1,'first'),:);
+Kfoot2 = kinect.lines(find(contains(orderK,"AnkleRight"),1,'first'),:);
 
-openpose = pointCloud([OPfoot1; OPfoot2; OPneck ]);
-kinect = pointCloud([Kfoot1; Kfoot2; Kneck ]);
+openposematch = [OPhead ;OPneck ;OPshoulder1 ;OPshoulder2 ;OPelbow1 ;OPelbow2 ;OPhand1 ;OPhand2 ;OPhip1 ;OPhip2 ;OPknee1 ;OPknee2 ;OPfoot1 ;OPfoot2];
+kinectmatch = [Khead ;Kneck ;Kshoulder1 ;Kshoulder2 ;Kelbow1 ;Kelbow2 ;Khand1 ;Khand2 ;Khip1 ;Khip2 ;Kknee1 ;Kknee2 ;Kfoot1 ;Kfoot2];
+
+% remove center of mass from all the points (move to 0,0)
+center = mean(openposematch);
+openposematch = openposematch - center;
+openposematch = pointCloud(openposematch);
+
+center = mean(kinectmatch);
+kinectmatch = kinectmatch - center;
+kinectmatch = pointCloud(kinectmatch);
+
+
+% OP_start2 = openposematch.Location(1:2:end,:);
+% OP_end2 = openposematch.Location(2:2:end,:);
+% 
+% kinect_start = kinectmatch.Location(1:2:end,:);
+% kinect_end = kinectmatch.Location(2:2:end,:);
 
 %calc transformation via icp
-tform = pcregrigid(openpose,kinect,'Extrapolate',true);
+tform = pcregrigid(openposematch,kinectmatch,'Extrapolate',true);
 %apply the transformation
 openpose_transformed = pctransform(pointCloud(OpenPoseSkeleton3D),tform);
 openpose_transformed = openpose_transformed.Location;
 
 OP_start2 = openpose_transformed(1:2:end,:);
 OP_end2 = openpose_transformed(2:2:end,:);
+
+
 
 % OPfoot = OpenPoseSkeleton3D(find(contains(orderOP,"LAnkle"),1,'first'),:);
 % % find the rotation matrix
